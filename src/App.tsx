74483a97e55119311cf29fe080f5bf84a1042708
LeonardoLobas <leonardolobas2703@gmail.com>
2025-04-23 14:07:18 -0300

    Implementa componente Button e lógica de incremento no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import React from "react";
 import "./main.css";
+import Button from "./Button";
 
 function App() {
-    return <div></div>;
+    const [total, setTotal] = React.useState(0);
+
+    function incrementar() {
+        setTotal(() => total + 1);
+    }
+
+    return (
+        <div>
+            {total}
+            <Button onClick={incrementar} tamanho="1.25rem">
+                Incrementar
+            </Button>
+        </div>
+    );
 }
 
 export default App;
